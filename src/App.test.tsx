c0f79a4df7383152ffe424abc254cbba2b0edbf3
DinGis9 <oleg777gorbachov@gmail.com>
2023-01-04 13:00:08 +0200

    asd

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,32 +1,41 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("email input should render", () => {
   render(<App />);
-  const linkElement = screen.getByPlaceholderText("Email");
-  expect(linkElement).toBeInTheDocument();
+  const emailInput = screen.getByPlaceholderText("Email");
+  expect(emailInput).toBeInTheDocument();
 });
 
 test("password input should render", () => {
   render(<App />);
-  const linkElement = screen.getByPlaceholderText("Password");
-  expect(linkElement).toBeInTheDocument();
+  const passlInput = screen.getByPlaceholderText("Password");
+  expect(passlInput).toBeInTheDocument();
 });
 
 test("submit button should be render", () => {
   render(<App />);
-  const linkElement = screen.getByDisplayValue("Submit");
-  expect(linkElement).toBeInTheDocument();
+  const btnElement = screen.getByDisplayValue("Submit");
+  expect(btnElement).toBeInTheDocument();
 });
 
 test("email input value should be empty", () => {
   render(<App />);
-  const linkElement = screen.getByPlaceholderText("Email");
-  expect(linkElement).toHaveValue("");
+  const emailInput = screen.getByPlaceholderText("Email");
+  expect(emailInput).toHaveValue("");
 });
 
 test("password input value should be empty", () => {
   render(<App />);
-  const linkElement = screen.getByPlaceholderText("Password");
-  expect(linkElement).toHaveValue("");
+  const passlInput = screen.getByPlaceholderText("Password");
+  expect(passlInput).toHaveValue("");
+});
+
+test("button onclick focus on email", () => {
+  render(<App />);
+  const emailInput = screen.getByPlaceholderText("Email");
+  const btnElement = screen.getByDisplayValue("Submit");
+
+  fireEvent.click(btnElement);
+  expect(emailInput).toHaveFocus();
 });
